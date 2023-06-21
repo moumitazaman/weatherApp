2a83074bd260244961d18304bf5fb4db4334ee37
moumitasub <moumitasub@gmail.com>
2023-06-21 11:50:44 +0600

    js edit

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ let desc = document.querySelector('.desc');
 let weatherIcon = document.querySelector('.icon');
 
 searchButton.addEventListener("click", function(){
+const location = city.value
+    getWeatherData(location)
 
-const url=`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=metric&appid=ad15d9db4bbca99e7c3d083640de25d1`
+})
+function getWeatherData(location){
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=ad15d9db4bbca99e7c3d083640de25d1`
     
-fetch(url)
+return fetch(url)
 .then(response => response.json())
 .then(getData)
 .catch(err => console.log(err)); 
 
-})
+ }
 
 const getData=(weather)=>{
     temp.innerText=`${weather.main.temp}°C`
